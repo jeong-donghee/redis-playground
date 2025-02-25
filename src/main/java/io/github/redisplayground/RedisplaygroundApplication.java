d96d5480c83f27208fa4477e75b9c69385c2a7e0
jeongdonghee <dh.jeong@ex-em.com>
2025-02-25 10:58:51 +0900

    Init project

---
+++ src/main/java/io/github/redisplayground/RedisplaygroundApplication.java
@@ -0,0 +1,13 @@
+package io.github.redisplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisplaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/redisplayground/RedisplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.redisplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
